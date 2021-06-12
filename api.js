beae638ac3549628c195a94a84baca4b0f48ed54
A Fish <ashley.paz.fish@gmail.com>
2021-06-12 12:20:16 -0500

    add api.js and adjustment to esLint file

---
+++ api.js
@@ -0,0 +1,11 @@
+// Example from class
+// const BASE_URL = "https://jsonplaceholder.typicode.com/todos";
+
+// export default {
+//   getTodos() {
+//     const todos = fetch(BASE_URL)
+//       .then((res) => res.json())
+//       .then((todos) => todos);
+//   },
+// };
+// End Example from class
