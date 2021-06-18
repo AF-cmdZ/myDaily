01c3f8a4a319eae9be65a1c7f4a3febcd80066ee
A Fish <ashley.paz.fish@gmail.com>
2021-06-17 20:04:13 -0500

    add missing api.js code from facts branch

--- api.js
+++ api.js
@@ -2,12 +2,29 @@ const dogFacts =
   "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1";
 const catFacts = "https://catfact.ninja/fact";
 
-fetch(catFacts).then(function (response) {
+fetch(catFacts)
+  .then(function (response) {
+    if (response.status === 200) {
+      console.log("GOT");
+    } else {
+      console.log("NOTOGT");
+    }
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.fact);
+    const df = data.fact;
+    document.getElementById("ff").innerHTML = df;
+  });
+
+fetch(dogFacts, { mode: "no-cors" }).then(function (response) {
   if (response.status === 200) {
     console.log("GOT");
+  } else {
+    console.log("NOTOGT");
   }
-
-  console.log(response);
 });
 
-// BONUS
+console.log("END");
+
+// BONUS: Custom "API"
