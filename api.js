8973c246005d475eb3d02fa4639fb3ac29f88a1c
unknown <alwferv@gmail.com>
2021-06-19 10:47:23 -0500

    Finished cat facts module

--- api.js
+++ api.js
@@ -9,5 +9,3 @@ export const getCatFacts = () => {
       return data;
     });
 };
-
-// BONUS: Custom "API"
--- scripts.js
+++ scripts.js
@@ -2,4 +2,6 @@ import { getCatFacts } from "./api.js";
 
 getCatFacts().then((catFacts) => {
   console.log(catFacts.fact);
+  const cf = catFacts.fact;
+  document.getElementById("animal-subtitle").innerHTML = cf;
 });
