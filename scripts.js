52c65501ea8db3ee7c60b9734542c569c2e7064a
unknown <alwferv@gmail.com>
2021-06-19 10:50:13 -0500

    Finished cat facts module

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 import { getCatFacts } from "./api.js";
 
 getCatFacts().then((catFacts) => {
-  console.log(catFacts.fact);
   const cf = catFacts.fact;
   document.getElementById("animal-subtitle").innerHTML = cf;
 });
